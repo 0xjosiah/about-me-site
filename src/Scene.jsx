6ea90fd64971be3904f45a7d9ee333f7b9b0d76c
Josiah Webb <jj.webb520@gmail.com>
2023-04-28 10:59:56 -0600

    fixes navs

--- src/Scene.jsx
+++ src/Scene.jsx
@@ -13,10 +13,10 @@ export default function Scene() {
     const { width, height } = useThree((state) => state.viewport)
 
     /* General purpose controls, attach to whatever for adjustment */
-    const { position, rotation } = useControls({
-        position: [ 0, 0, 0 ],
-        rotation: [ 0, 0, 0 ],
-    })
+    // const { position, rotation } = useControls({
+    //     position: [ 0, 0, 0 ],
+    //     rotation: [ 0, 0, 0 ],
+    // })
 
     /* Camera position if needed */
     // const { position } = useThree((state) => state.camera)
@@ -104,7 +104,7 @@ export default function Scene() {
             <ScrollControls pages={8} infinite horizontal>
                 <Scroll>
 
-                    <SectionTitle text='BEGINNINGS' navTo='/about' position={[ conditionalWidth, 2, 2 ]}>
+                    <SectionTitle text='BEGINNINGS' navTo='/' position={[ conditionalWidth, 2, 2 ]}>
                         {/* <Plane args={[picWidth, picHeight]} position={[ -1, -1, 1 ]} ref={planeRef}>
                             <meshBasicMaterial map={texture} />
                         </Plane> */}
@@ -116,7 +116,7 @@ export default function Scene() {
                     </SectionTitle>
                     
 
-                    <SectionTitle text='COLLEGE' navTo='/experience' position={[ width * 2, 2, 2 ]}>
+                    <SectionTitle text='COLLEGE' navTo='/' position={[ width * 2, 2, 2 ]}>
                         <Pic image={buffsBball} position={[ -0.04, -0.92, 0.86 ]} size={1.2}/>
                         <Pic image={stadiumShot} position={[ -0.04, -.2, -1.86 ]} size={1.2}/>
                         <Pic image={paintProj} position={[ 4, -1.2, -0.045 ]} size={1.2}/>
@@ -125,7 +125,7 @@ export default function Scene() {
                         <Pic image={collegeGrad} position={[ 7.97, -0.75, 0.4 ]} size={1.2}/>
                     </SectionTitle>
 
-                    <SectionTitle text='POST GRAD' navTo='/projects' position={[ width * 3, 2, 2 ]}>
+                    <SectionTitle text='POST GRAD' navTo='/' position={[ width * 3, 2, 2 ]}>
                         <Pic image={denverSign} position={[ -0.04, -0.92, 0.86 ]} size={1.2}/>
                         <Pic image={headShot} position={[ -0.04, -.2, -1.86 ]} size={1.2}/>
                         <Pic image={stonks} position={[ 4, -1.2, -0.045 ]} size={1.2}/>
@@ -134,7 +134,7 @@ export default function Scene() {
                         {/* <Pic image={notStonks} position={[ 7.97, -0.75, 0.4 ]} size={1.2}/> */}
                     </SectionTitle>
 
-                    <SectionTitle text='2020' navTo='/projects' position={[ width * 4, 2, 2 ]}>
+                    <SectionTitle text='2020' navTo='/' position={[ width * 4, 2, 2 ]}>
                         <Pic image={notStonks} position={[ -0.04, -0.92, 0.86 ]} size={1.2}/>
                         <Pic image={bikeAspens} position={[ -0.04, -.2, -1.86 ]} size={1.2}/>
                         <Pic image={bikeFourteener} position={[ 4, -1.2, -0.045 ]} size={1.2}/>
@@ -150,7 +150,7 @@ export default function Scene() {
                         <Pic image={galaxyGenerator} position={[ 5.5, -0.17, 0.5 ]} size={1.2}/>
                     </SectionTitle>
 
-                    <SectionTitle text='NEXT...' navTo='/projects' position={[ width * 6, 2, 2 ]}></SectionTitle>
+                    <SectionTitle text='NEXT...' navTo='/' position={[ width * 6, 2, 2 ]}></SectionTitle>
 
                 </Scroll>
             </ScrollControls>
